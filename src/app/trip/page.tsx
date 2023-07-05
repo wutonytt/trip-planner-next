b2dd7da7c241b7704952342eebe12e1fed5239b5
Tony Wu <wutonytt@gmail.com>
2023-07-05 22:41:54 +0800

    feat: add suspense to show loading UI on trip list

--- src/app/trip/page.tsx
+++ src/app/trip/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
 import TripList from "./TripList";
+import LoadingSkeleton from "./LoadingSkeleton";
 
 export default function TripPage() {
   return (
     <div>
       <div className="p-5">
-        <TripList />
+        <Suspense fallback={<LoadingSkeleton />}>
+          <TripList />
+        </Suspense>
       </div>
     </div>
   );
